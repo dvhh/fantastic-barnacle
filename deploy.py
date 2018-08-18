d6cf96f0cb8f53fa871b1f794daa3725a5a7a878
dvhh <dvhh@users.noreply.github.com>
2018-08-18 17:02:58 +0900

    Create deploy.py

---
+++ deploy.py
@@ -0,0 +1,10 @@
+import os
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+cur = conn.cursor()
+
+with open('create.sql', 'r') as fd
+  cur.execute(fd.read)
