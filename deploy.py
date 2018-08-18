055d0f576260530f2fc5305a47c64a2686c35227
dvhh <dvhh@users.noreply.github.com>
2018-08-18 17:05:40 +0900

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -6,5 +6,5 @@ DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
 
-with open('create.sql', 'r') as fd
+with open('create.sql', 'r') as fd:
   cur.execute(fd.read)
